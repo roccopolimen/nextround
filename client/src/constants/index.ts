c9f7fa75bd36f82cbaca02e6b3a2e0e30b829764
Rocco Polimeni <rpolimen@stevens.edu>
2022-03-28 11:49:52 -0400

    bring back theme for website

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,10 +1,11 @@
 import { createTheme, Theme } from "@mui/material";
 
-// TODO: actually create a theme
 const theme: Theme = createTheme({
     palette: {
-        mode: 'dark',
-    }
+        primary: {
+          main: '#6c50ed',
+        }
+    },
 });
 
 export default theme;
