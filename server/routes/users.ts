05421d4d1275513b2557cee30cd976c600c0ba23
Rocco Polimeni <rpolimen@stevens.edu>
2022-05-08 17:54:07 -0400

    removed outdated imports

--- server/routes/users.ts
+++ server/routes/users.ts
@@ -1,8 +1,8 @@
 import express from 'express';
 import { DecodedIdToken, getAuth } from "firebase-admin/auth";
 import { ObjectId } from 'mongodb';
-import { createUser, getUserByEmail, getUserById, removeUser, updateUser } from '../data';
-import { checkEmail, checkNonEmptyString, checkURL } from '../helpers';
+import { createUser, getUserByEmail, getUserById, removeUser } from '../data';
+import { checkNonEmptyString } from '../helpers';
 import { UserObject } from '../typings';
 
 const router = express.Router();
